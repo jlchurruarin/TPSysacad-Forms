8f14555e07990b4ae0cf5afa1c15da843a342581
José Churruarin <jlchurruarin@gmail.com>
2023-10-01 16:02:57 -0300

    +Base Datos personales

--- formABMEstudiante.Designer.cs
+++ formABMEstudiante.Designer.cs
@@ -29,16 +29,35 @@
         private void InitializeComponent()
         {
             tlpEstudiante = new TableLayoutPanel();
+            btnCancelar = new Button();
             tacEstudiante = new TabControl();
             tabDatosPersonales = new TabPage();
+            tlpDatosPersonales = new TableLayoutPanel();
             tabCursosInscriptos = new TabPage();
             tabPagos = new TabPage();
             btnGuardar = new Button();
-            btnCancelar = new Button();
-            tlpDatosPersonales = new TableLayoutPanel();
+            lblNombre = new Label();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
             tlpEstudiante.SuspendLayout();
             tacEstudiante.SuspendLayout();
             tabDatosPersonales.SuspendLayout();
+            tlpDatosPersonales.SuspendLayout();
             SuspendLayout();
             // 
             // tlpEstudiante
@@ -59,6 +78,16 @@
             tlpEstudiante.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
             tlpEstudiante.Size = new Size(827, 536);
             tlpEstudiante.TabIndex = 0;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Anchor = AnchorStyles.None;
+            btnCancelar.Location = new Point(714, 63);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(75, 23);
+            btnCancelar.TabIndex = 2;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
             // 
             // tacEstudiante
             // 
@@ -83,6 +112,46 @@
             tabDatosPersonales.TabIndex = 0;
             tabDatosPersonales.Text = "Datos Personales";
             tabDatosPersonales.UseVisualStyleBackColor = true;
+            // 
+            // tlpDatosPersonales
+            // 
+            tlpDatosPersonales.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tlpDatosPersonales.ColumnCount = 3;
+            tlpDatosPersonales.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tlpDatosPersonales.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tlpDatosPersonales.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 150F));
+            tlpDatosPersonales.Controls.Add(lblNombre, 0, 0);
+            tlpDatosPersonales.Controls.Add(textBox1, 1, 0);
+            tlpDatosPersonales.Controls.Add(label1, 0, 1);
+            tlpDatosPersonales.Controls.Add(label2, 0, 2);
+            tlpDatosPersonales.Controls.Add(label3, 0, 3);
+            tlpDatosPersonales.Controls.Add(label4, 0, 4);
+            tlpDatosPersonales.Controls.Add(label5, 0, 5);
+            tlpDatosPersonales.Controls.Add(label6, 0, 6);
+            tlpDatosPersonales.Controls.Add(label7, 0, 7);
+            tlpDatosPersonales.Controls.Add(textBox2, 1, 1);
+            tlpDatosPersonales.Controls.Add(textBox3, 1, 2);
+            tlpDatosPersonales.Controls.Add(textBox4, 1, 3);
+            tlpDatosPersonales.Controls.Add(textBox5, 1, 4);
+            tlpDatosPersonales.Controls.Add(textBox6, 1, 5);
+            tlpDatosPersonales.Controls.Add(textBox7, 1, 6);
+            tlpDatosPersonales.Controls.Add(textBox8, 1, 7);
+            tlpDatosPersonales.Controls.Add(checkBox1, 1, 8);
+            tlpDatosPersonales.Controls.Add(checkBox2, 2, 7);
+            tlpDatosPersonales.Location = new Point(6, 6);
+            tlpDatosPersonales.Name = "tlpDatosPersonales";
+            tlpDatosPersonales.RowCount = 9;
+            tlpDatosPersonales.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tlpDatosPersonales.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tlpDatosPersonales.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tlpDatosPersonales.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tlpDatosPersonales.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tlpDatosPersonales.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tlpDatosPersonales.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tlpDatosPersonales.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tlpDatosPersonales.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tlpDatosPersonales.Size = new Size(651, 490);
+            tlpDatosPersonales.TabIndex = 0;
             // 
             // tabCursosInscriptos
             // 
@@ -113,34 +182,177 @@
             btnGuardar.Text = "Guardar";
             btnGuardar.UseVisualStyleBackColor = true;
             // 
-            // btnCancelar
+            // lblNombre
             // 
-            btnCancelar.Anchor = AnchorStyles.None;
-            btnCancelar.Location = new Point(714, 63);
-            btnCancelar.Name = "btnCancelar";
-            btnCancelar.Size = new Size(75, 23);
-            btnCancelar.TabIndex = 2;
-            btnCancelar.Text = "Cancelar";
-            btnCancelar.UseVisualStyleBackColor = true;
+            lblNombre.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            lblNombre.AutoSize = true;
+            lblNombre.Location = new Point(3, 0);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(244, 30);
+            lblNombre.TabIndex = 0;
+            lblNombre.Text = "Nombre";
+            lblNombre.TextAlign = ContentAlignment.MiddleCenter;
             // 
-            // tlpDatosPersonales
+            // textBox1
             // 
-            tlpDatosPersonales.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            tlpDatosPersonales.ColumnCount = 3;
-            tlpDatosPersonales.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tlpDatosPersonales.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tlpDatosPersonales.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 150F));
-            tlpDatosPersonales.Location = new Point(6, 6);
-            tlpDatosPersonales.Name = "tlpDatosPersonales";
-            tlpDatosPersonales.RowCount = 6;
-            tlpDatosPersonales.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
-            tlpDatosPersonales.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
-            tlpDatosPersonales.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
-            tlpDatosPersonales.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
-            tlpDatosPersonales.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
-            tlpDatosPersonales.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
-            tlpDatosPersonales.Size = new Size(651, 490);
-            tlpDatosPersonales.TabIndex = 0;
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Location = new Point(253, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(244, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(244, 30);
+            label1.TabIndex = 2;
+            label1.Text = "Apellido";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Location = new Point(3, 60);
+            label2.Name = "label2";
+            label2.Size = new Size(244, 30);
+            label2.TabIndex = 3;
+            label2.Text = "DNI";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label3.AutoSize = true;
+            label3.Location = new Point(3, 90);
+            label3.Name = "label3";
+            label3.Size = new Size(244, 30);
+            label3.TabIndex = 4;
+            label3.Text = "Legajo:";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label4.AutoSize = true;
+            label4.Location = new Point(3, 120);
+            label4.Name = "label4";
+            label4.Size = new Size(244, 30);
+            label4.TabIndex = 5;
+            label4.Text = "Numero de telefono:";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label5.AutoSize = true;
+            label5.Location = new Point(3, 150);
+            label5.Name = "label5";
+            label5.Size = new Size(244, 30);
+            label5.TabIndex = 6;
+            label5.Text = "Dirección:";
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            label6.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label6.AutoSize = true;
+            label6.Location = new Point(3, 180);
+            label6.Name = "label6";
+            label6.Size = new Size(244, 30);
+            label6.TabIndex = 7;
+            label6.Text = "Correo Electronico:";
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            label7.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label7.AutoSize = true;
+            label7.Location = new Point(3, 210);
+            label7.Name = "label7";
+            label7.Size = new Size(244, 30);
+            label7.TabIndex = 8;
+            label7.Text = "Contraseña:";
+            label7.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox2.Location = new Point(253, 33);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(244, 23);
+            textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            textBox3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox3.Location = new Point(253, 63);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(244, 23);
+            textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            textBox4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox4.Location = new Point(253, 93);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(244, 23);
+            textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            textBox5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox5.Location = new Point(253, 123);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(244, 23);
+            textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            textBox6.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox6.Location = new Point(253, 153);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(244, 23);
+            textBox6.TabIndex = 13;
+            // 
+            // textBox7
+            // 
+            textBox7.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox7.Location = new Point(253, 183);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(244, 23);
+            textBox7.TabIndex = 14;
+            // 
+            // textBox8
+            // 
+            textBox8.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox8.Location = new Point(253, 213);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(244, 23);
+            textBox8.TabIndex = 15;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(253, 243);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(83, 19);
+            checkBox1.TabIndex = 16;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(503, 213);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(83, 19);
+            checkBox2.TabIndex = 17;
+            checkBox2.Text = "checkBox2";
+            checkBox2.UseVisualStyleBackColor = true;
             // 
             // formABMEstudiante
             // 
@@ -153,6 +365,8 @@
             tlpEstudiante.ResumeLayout(false);
             tacEstudiante.ResumeLayout(false);
             tabDatosPersonales.ResumeLayout(false);
+            tlpDatosPersonales.ResumeLayout(false);
+            tlpDatosPersonales.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -166,5 +380,23 @@
         private TabPage tabCursosInscriptos;
         private TabPage tabPagos;
         private Button btnGuardar;
+        private Label lblNombre;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
     }
 }
