5319852ccefbb9120dedc6476fd878c0375f9534
José Churruarin <jlchurruarin@gmail.com>
2023-10-06 22:09:41 -0300

    Se modifica constructor

--- formABMCurso.cs
+++ formABMCurso.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BibliotecaClases;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,46 @@ namespace TPSysacad___Forms
 {
     public partial class formABMCurso : Form
     {
-        public formABMCurso()
+        private Form _formAnterior;
+        private Curso _curso;
+        private BaseDeDatos _baseDeDatos;
+
+        public formABMCurso(Form formAnterior, Curso curso, BaseDeDatos baseDeDatos)
         {
+            _formAnterior = formAnterior;
+            _curso = curso;
+            _baseDeDatos = baseDeDatos;
             InitializeComponent();
+        }
+
+        private void formABMCurso_Load(object sender, EventArgs e)
+        {
+            CargarCursoEnForm();
+        }
+
+        private void CargarCursoEnForm()
+        {
+            if (_curso is null) { return; }
+
+
+
+
+
+        }
+
+        private void CargarListaDeHorarios()
+        {
+
+        }
+
+        private void CargarListaEstudiantesInscriptos()
+        {
+
+        }
+
+        private void CargarListaEstudiantesEnEspera()
+        {
+
         }
     }
 }
