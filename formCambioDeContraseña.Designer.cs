e2109e724269048619a8b8f1dab5036c814b6b6e
José Churruarin <jlchurruarin@gmail.com>
2023-10-08 18:10:02 -0300

    A: Form cambio de contraseña

---
+++ formCambioDeContraseña.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace TPSysacad___Forms
+{
+    partial class formCambioDeContraseña
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            btnCancelar = new Button();
+            lblTitulo = new Label();
+            lblContraseñaUno = new Label();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            btnAceptar = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(btnCancelar, 1, 3);
+            tableLayoutPanel1.Controls.Add(lblTitulo, 0, 0);
+            tableLayoutPanel1.Controls.Add(lblContraseñaUno, 0, 1);
+            tableLayoutPanel1.Controls.Add(label1, 0, 2);
+            tableLayoutPanel1.Controls.Add(textBox1, 1, 1);
+            tableLayoutPanel1.Controls.Add(textBox2, 1, 2);
+            tableLayoutPanel1.Controls.Add(btnAceptar, 0, 3);
+            tableLayoutPanel1.Location = new Point(12, 12);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 4;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 40F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 40F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 40F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 40F));
+            tableLayoutPanel1.Size = new Size(381, 165);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Anchor = AnchorStyles.None;
+            btnCancelar.Location = new Point(235, 128);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(101, 29);
+            btnCancelar.TabIndex = 6;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // lblTitulo
+            // 
+            lblTitulo.Anchor = AnchorStyles.None;
+            lblTitulo.AutoSize = true;
+            tableLayoutPanel1.SetColumnSpan(lblTitulo, 2);
+            lblTitulo.Location = new Point(98, 12);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(184, 15);
+            lblTitulo.TabIndex = 0;
+            lblTitulo.Text = "Se requiere cambiar la contraseña";
+            // 
+            // lblContraseñaUno
+            // 
+            lblContraseñaUno.Anchor = AnchorStyles.None;
+            lblContraseñaUno.AutoSize = true;
+            lblContraseñaUno.Location = new Point(42, 52);
+            lblContraseñaUno.Name = "lblContraseñaUno";
+            lblContraseñaUno.Size = new Size(105, 15);
+            lblContraseñaUno.TabIndex = 1;
+            lblContraseñaUno.Text = "Nueva contraseña:";
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.None;
+            label1.AutoSize = true;
+            label1.Location = new Point(15, 92);
+            label1.Name = "label1";
+            label1.Size = new Size(160, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Confirmación de contraseña:";
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.None;
+            textBox1.Location = new Point(193, 48);
+            textBox1.Name = "textBox1";
+            textBox1.PasswordChar = '*';
+            textBox1.Size = new Size(185, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.None;
+            textBox2.Location = new Point(193, 88);
+            textBox2.Name = "textBox2";
+            textBox2.PasswordChar = '*';
+            textBox2.Size = new Size(185, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // btnAceptar
+            // 
+            btnAceptar.Anchor = AnchorStyles.None;
+            btnAceptar.Location = new Point(44, 128);
+            btnAceptar.Name = "btnAceptar";
+            btnAceptar.Size = new Size(101, 29);
+            btnAceptar.TabIndex = 5;
+            btnAceptar.Text = "Aceptar";
+            btnAceptar.UseVisualStyleBackColor = true;
+            btnAceptar.Click += btnAceptar_Click;
+            // 
+            // fromCambioDeContraseña
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(405, 189);
+            Controls.Add(tableLayoutPanel1);
+            Name = "fromCambioDeContraseña";
+            Text = "fromCambioDeContraseña";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label lblTitulo;
+        private Button btnCancelar;
+        private Label lblContraseñaUno;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button btnAceptar;
+    }
+}
---
+++ formCambioDeContraseña.cs
@@ -0,0 +1,51 @@
+﻿using BibliotecaClases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPSysacad___Forms
+{
+    public partial class formCambioDeContraseña : Form
+    {
+        private Usuario _usuario;
+
+        public formCambioDeContraseña(Usuario usuario)
+        {
+            _usuario = usuario;
+            InitializeComponent();
+        }
+
+        private bool ValidarContraseñasIguales()
+        {
+            if (textBox1.Text == textBox2.Text)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (ValidarContraseñasIguales())
+            {
+                _usuario.Contraseña = Sistema.EncriptarTexto(textBox1.Text);
+                _usuario.CambioDeContraseñaObligatorio = false;
+                MessageBox.Show("Contraseña Modificada, por favor vuelva a ingresar", "Cambio de contraseña");
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Las contraseñas ingresadas no son iguales");
+            }
+        }
+    }
+}
